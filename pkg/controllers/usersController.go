0cf3a8fcbab7d3f37323a3f0e1ca482825a012b0
Bakhodur <bakhodur.nazriev@gmail.com>
2022-05-07 19:01:35 +0500

    Create and Implement users model and controller.

--- pkg/controllers/usersController.go
+++ pkg/controllers/usersController.go
@@ -1,13 +1,88 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/bakhodur-nazriev/my-blog/pkg/models"
+	"github.com/bakhodur-nazriev/my-blog/utils"
+	"github.com/gorilla/mux"
+	"net/http"
+	"strconv"
+)
 
-func GetAllUsers(w http.ResponseWriter, r *http.Request) {}
+var NewUser models.Users
 
-func GetUserById(w http.ResponseWriter, r *http.Request) {}
+func GetAllUsers(w http.ResponseWriter, r *http.Request) {
+	newUser := models.GetAllUsers()
+	res, _ := json.Marshal(newUser)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
 
-func CreateUser(w http.ResponseWriter, r *http.Request) {}
+func GetUserById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	userId := vars["userId"]
+	ID, err := strconv.ParseInt(userId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	userDetails, _ := models.GetUserById(ID)
+	res, _ := json.Marshal(userDetails)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
 
-func UpdateUser(w http.ResponseWriter, r *http.Request) {}
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	CreateUser := &models.Users{}
+	utils.ParseBody(r, CreateUser)
+	u := CreateUser.CreateUser()
+	res, _ := json.Marshal(u)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
 
-func DeleteUser(w http.ResponseWriter, r *http.Request) {}
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	var updateUser = &models.Users{}
+	utils.ParseBody(r, updateUser)
+	vars := mux.Vars(r)
+	userId := vars["userId"]
+	ID, err := strconv.ParseInt(userId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	userDetails, db := models.GetUserById(ID)
+	if updateUser.Name != "" {
+		userDetails.Name = updateUser.Name
+	}
+
+	if updateUser.Email != "" {
+		userDetails.Email = updateUser.Email
+	}
+
+	if updateUser.Password != "" {
+		userDetails.Password = updateUser.Password
+	}
+
+	db.Save(&userDetails)
+	res, _ := json.Marshal(userDetails)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	userId := vars["userId"]
+	ID, err := strconv.ParseInt(userId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	user := models.DeleteUser(ID)
+	res, _ := json.Marshal(user)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
--- pkg/models/users.go
+++ pkg/models/users.go
@@ -1,1 +1,43 @@
 package models
+
+import (
+	"github.com/bakhodur-nazriev/my-blog/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+
+type Users struct {
+	gorm.Model
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Users{})
+}
+
+func GetAllUsers() []Users {
+	var Users []Users
+	db.Find(&Users)
+	return Users
+}
+
+func GetUserById(Id int64) (*Users, *gorm.DB) {
+	var getUser Users
+	db := db.Where("ID=?", Id).Find(&getUser)
+	return &getUser, db
+}
+
+func (u *Users) CreateUser() *Users {
+	db.NewRecord(u)
+	db.Create(&u)
+	return u
+}
+
+func DeleteUser(Id int64) Users {
+	var user Users
+	db.Where("ID=?", Id).Delete(user)
+	return user
+}
