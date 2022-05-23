932f3df9f35c6e68b4567f23fbe5f90c3a01a27a
Bakhodur Nazriev <bakhodur.nazriev@gmail.com>
2022-05-23 17:47:47 +0500

    Improve App.vue

--- front-end/vuejs/src/App.vue
+++ front-end/vuejs/src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <navbar-component></navbar-component>
+  <router-view/>
+  <footer-component></footer-component>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavbarComponent from "@/components/NavbarComponent";
+import FooterComponent from "@/components/FooterComponent";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    NavbarComponent,
+    FooterComponent
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Red+Hat+Display:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap');
+
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Red Hat Display", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
