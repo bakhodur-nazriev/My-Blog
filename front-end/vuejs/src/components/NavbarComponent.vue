8a9a1cf0bcc17b7db80cb3b51142018e15fec8ff
Bakhodur Nazriev <bakhodur.nazriev@gmail.com>
2022-05-23 18:00:47 +0500

    Improve App.vue

--- front-end/vuejs/src/components/NavbarComponent.vue
+++ front-end/vuejs/src/components/NavbarComponent.vue
@@ -1,10 +1,19 @@
 <template>
-  <nav>
-    <router-link class="test" to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
-    <router-link to="/posts">Posts</router-link>
-    <router-link to="/authors">Authors</router-link>
-    <router-link to="/contact">Contact</router-link>
+  <nav class="main-navbar">
+    <div>
+      <img height="70" src="../assets/logo_blog.png" alt="logo">
+    </div>
+    <div>
+      <router-link class="test" to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+      <router-link to="/posts">Posts</router-link>
+      <router-link to="/authors">Authors</router-link>
+      <router-link to="/contact">Contact</router-link>
+    </div>
+
+    <div>
+      <input type="text" class="form-control" placeholder="Search...">
+    </div>
   </nav>
 </template>
 
@@ -18,14 +27,16 @@ export default {
 
 <style>
 nav {
+  display: flex;
+  justify-content: space-around;
   padding: 30px;
 }
 
 nav a {
-  padding: 0 20px;
-  font-weight: bold;
   color: #878787;
   text-decoration: none;
+  margin: 0 18px;
+  font-weight: 500;
 }
 
 nav a.router-link-exact-active {
